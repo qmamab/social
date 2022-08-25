4c4a632e8dc05e85e3965162553e1c2540d5705b
qamar muneer akbar <62990005+qmamab@users.noreply.github.com>
2022-08-25 06:20:09 -0700

    Add files via upload

---
+++ network/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from network.models import Post, User, Profile
+# Register your models here.
+
+
+admin.site.register(Post)
+admin.site.register(User)
+admin.site.register(Profile)
+
+#propertyofqma
