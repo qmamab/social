21aafe837f18d742ac1c4208c376836206118420
qamar muneer akbar <62990005+qmamab@users.noreply.github.com>
2022-08-25 06:44:14 -0700

    Update urls.py

--- project4/urls.py
+++ project4/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("network.urls")),
-]
+    path('admin/', admin.site.urls),
+    path('', include("Home.urls")),
 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+]
 #propertyofqma
